5c7564dad8e2759ea6853657742f8a9aa80f8493
John Paul <lucas.gabule@gmail.com>
2023-03-19 01:50:04 +0800

    adding good solution on array problem question 2

--- arrays/04_.js
+++ arrays/04_.js
@@ -10,5 +10,23 @@ const maxAreaBruteForce = (array) => {
   return area;
 }
 
-console.log(maxAreaBruteForce([3, 7, 5, 6, 8, 4]))
-console.log(maxAreaBruteForce([9, 1, 2, 3, 10]))
+//console.log(maxAreaBruteForce([3, 7, 5, 6, 8, 4]))
+//console.log(maxAreaBruteForce([9, 1, 2, 3, 10]))
+
+const maxArea = (array) => {
+  let area = 0;
+  let i = 0;
+  let j = array.length - 1;
+
+  while( i < j) {
+    let height = Math.min(array[i], array[j])
+    let newArea = height * (j - i)
+    area = Math.max(area, newArea)
+    if(array[i] < array[j]) i++;
+    else j--;
+  }
+  return area;
+}
+
+console.log(maxArea([3, 7, 5, 6, 8, 4]))
+console.log(maxArea([9, 1, 2, 3, 10]))
